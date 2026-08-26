4fb6053f0adc5aa47aab7a1227e157187f95d1c4
JIN <Gamdjin26@gmail.com>
2026-08-26 12:57:46 +0900

    C# 연습

--- 26031023-Practice/Program.cs
+++ 26031023-Practice/Program.cs
@@ -1,10 +1,15 @@
 ﻿namespace _26031023_Pratice
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(10, 5);
+            Console.WriteLine("나의 이력서");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(10, 5);
+            Console.WriteLine("이름 이진호");
         }
     }
 }
