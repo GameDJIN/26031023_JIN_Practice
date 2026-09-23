25d4315695c23e36faad5cb66d308ae01bd9c078
JIN <gamdjin26@gmail.com>
2026-09-23 12:44:02 +0900

    기능

---
+++ 26031023_LeeJinho_BasicTraing/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System.Data;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //string name = "이진호";
+        //string id = "26031023";
+        //string _subject = "객체지향C#";
+
+        //Console.WriteLine($"이름: {name}");
+        //Console.WriteLine($"학번: {id}");
+        //Console.WriteLine($"과목: {_subject}");
+
+        // 연산연습
+        //Console.WriteLine(10 + 2000);
+        //Console.WriteLine(10 + "2000");
+        //Console.WriteLine(Int32.Parse("10") + 2000);
+        //Console.WriteLine("10" + "2000");
+
+        //Console.WriteLine(12345 % 1000);
+
+        //문자형
+        //Console.WriteLine('가');
+
+        //문자열
+        //Console.WriteLine("이력서");
+        //Console.WriteLine("\n이름: 이진호");
+
+        Console.WriteLine("어"+"\uAE00");
+        int id = 1235;
+        char character= '가';
+        string massage= "안녕하세요.";
+    }
+}
+
+/*
+ * 
+ //생성
+Create();
+
+//초기화
+Init();
+
+//갱신
+Update();
+
+//렌더링
+Render();
+
+//파괴
+Destroy();
+
+ */
---
+++ glc2dTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace glc2dTest
+{
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Hello, World!");
+    //    }
+    //}
+}
